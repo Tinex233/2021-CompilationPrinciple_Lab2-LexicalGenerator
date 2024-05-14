0c9c7c7cf1ba4fa7e0171018e43f1bfed948cb73
Tinex233 <128806135+Tinex233@users.noreply.github.com>
2024-05-14 17:09:54 +0800

    1.3

---
+++ LexicalGenerator (Latest)/mainwindow.h
@@ -0,0 +1,61 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QString>
+#include <QMessageBox>
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QTextEdit>
+#include <QDebug>
+#include <QTextBlock>
+#include <QStringList>
+#include <QList>
+#include <QTextCodec>
+#include <QTableWidget>
+#include <QIcon>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void NFA_clicked();
+
+    void DFA_clicked();
+
+    void MinDFA_clicked();
+
+    void PG_clicked();
+
+    void on_CREATEbtn_clicked();
+
+    void on_SAVEbtn_clicked();
+
+    void on_savePGbtn_clicked();
+
+    void on_OPENbtn_clicked();
+
+    void on_LEFTbtn_clicked();
+
+    void on_RIGHTbtn_clicked();
+
+    void on_tabWidget_tabBarClicked(int index);
+
+private:
+    Ui::MainWindow *ui;
+
+    void SaveFile(QTextEdit *txted);
+
+    void showRes();
+};
+#endif // MAINWINDOW_H
